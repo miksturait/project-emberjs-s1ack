637a23ffd9fc4e5f1896affc4b182d525c836489
Michał Czyż <m.czyz@selleo.com>
2017-10-19 14:20:23 +0200

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,15 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:ember/recommended'],
   env: {
     browser: true
   },
   rules: {
+    'no-console': [1, { allow: ['warn', 'error'] }],
+    'comma-dangle': [2, 'only-multiline'],
+    'space-before-function-paren': 'off',
+    'no-extra-semi': 2,
+    quotes: [2, 'single', 'avoid-escape'],
   }
 };
