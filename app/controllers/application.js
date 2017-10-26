91c481809ffcaa6402273a7877fb8b014221126d
Michał Czyż <m.czyz@selleo.com>
2017-10-26 17:20:36 +0200

    create or update user object in database (based on session data)

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
-import { alias } from '@ember/object/computed'
-import { inject as service } from '@ember/service'
+import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   store: service(),
--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,4 +1,25 @@
 import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
+import { inject as service } from '@ember/service';
 
 export default ToriiFirebaseAdapter.extend({
+  store: service(),
+  open(authentication) {
+    return Ember.RSVP.hash({
+      uid: authentication.uid,
+      currentUser: this._findOrCreateUser(authentication)
+    })
+  },
+  _findOrCreateUser({uid, photoURL: photoUrl, displayName: name}) {
+    let store = this.get('store');
+
+    return store.findRecord('user', uid)
+      .then( function (user) {
+        user.setProperties(photoUrl, name)
+        return user.save()
+      })
+      .catch(function() {
+        let user = store.createRecord('user', {photoUrl, name, id: uid});
+        return user.save();
+      })
+  }
 });
