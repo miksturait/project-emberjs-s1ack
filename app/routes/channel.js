d73499c59c5fd14ac28780f4d8de2ddf01bb89bb
Michał Czyż <m.czyz@selleo.com>
2017-11-09 17:30:31 +0100

    channel page

--- app/routes/channel.js
+++ app/routes/channel.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    this.store.find('channel', params.channel_id)
+    return this.store.find('channel', params.channel_id)
   }
 });
