46ddf43e317090c65aecc7a60bfca23df09056f4
Michał Czyż <m.czyz@selleo.com>
2017-12-21 18:14:00 +0100

    Expand user model and edit functionality

--- app/models/user.js
+++ app/models/user.js
@@ -4,5 +4,11 @@ export default DS.Model.extend({
   uid: DS.attr('string'),
   name: DS.attr('string'),
   photoUrl: DS.attr('string'),
-  nick: DS.attr('string')
+  nick: DS.attr('string'),
+  whatIDo: DS.attr('string'),
+  status: DS.attr('string'),
+  timezone: DS.attr('string'),
+  secondaryEmail: DS.attr('string'),
+  street: DS.attr('string'),
+  city: DS.attr('string'),
 });
