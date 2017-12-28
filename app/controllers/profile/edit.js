144c138a7053bd9bcdef6861ac0b7228e720bb79
Michał Czyż <m.czyz@selleo.com>
2017-12-28 16:15:45 +0100

    after succesfull save, transition to profile

--- app/controllers/profile/edit.js
+++ app/controllers/profile/edit.js
@@ -1,9 +1,13 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
 
 export default Controller.extend({
+  router: service(),
   actions: {
     save() {
       this.model.save();
+      get(this, 'router').transitionTo('profile');
     }
   }
 });
