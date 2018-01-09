68a5b46cbc990e63f929014778c6141ffc76ed14
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-09 19:41:24 +0100

    Add component x-form/select2, delete it's template and tests

---
+++ app/components/x-form/select2.js
@@ -0,0 +1,28 @@
+import Select from 's1ack/components/x-form/select';
+import layout from 's1ack/templates/components/x-form/select'; 
+import $ from 'jquery';
+import { get, set } from '@ember/object';
+
+export default Select.extend({
+  layout,
+  didInsertElement(){
+    this._super(...arguments);
+    const options = get(this, 'options'),
+          value = get(this, 'value');
+          
+    const data = options.map(option => {
+     return value == option.id ? Object.assign({selected: true}, option) : option; // user's value is selected by default
+    });
+
+    $('select').select2({ 
+      width: 460,
+      data: data,
+    });
+
+    $('select').on('select2:select', (e) => {
+      const value = e.target.value;
+
+      set(this, 'value', value);
+    });
+  }
+})
