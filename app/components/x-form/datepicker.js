be51e9b0778c0c74f6ff7fb2d5413d17e73b4f03
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-18 00:01:35 +0100

    Define dateFormat as property of the Datepicker component

--- app/components/x-form/datepicker.js
+++ app/components/x-form/datepicker.js
@@ -1,14 +1,16 @@
 import Date from 's1ack/components/x-form/date';
-import { get, set} from '@ember/object';
+import { get, set } from '@ember/object';
 
 export default Date.extend({
   inputType: 'text',
+  dateFormat: 'YYYY-MM-DD',
   didInsertElement(){
     this._super(...arguments);
+    const dateFormat = get(this, 'dateFormat');
 
     set(this, 'pikaday', new Pikaday({
            field: this.element.querySelector('input'), // or we can use JQuery 
-           format: 'YYYY-MM-DD',
+           format: dateFormat,
            position: 'top right',
            showDaysInNextAndPreviousMonths: true,
       }))
