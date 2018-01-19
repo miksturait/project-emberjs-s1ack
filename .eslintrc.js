fa2f0354ea62c0bae255e6342e74274505554f54
BobrImperator <lordboberro@gmail.com>
2018-01-19 17:48:47 +0100

    allow usage of debugger, simpleArrowFunctions

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,14 @@ module.exports = {
   },
   rules: {
     'no-console': [1, { allow: ['warn', 'error'] }],
+    'no-debugger': 0,
     'comma-dangle': [2, 'only-multiline'],
     'space-before-function-paren': 'off',
     'no-extra-semi': 2,
     quotes: [2, 'single', 'avoid-escape'],
     'ember/alias-model-in-controller': 'off',
+    'ember/named-functions-in-promises':[2, {
+      allowSimpleArrowFunction: true,
+    }]
   }
 };
