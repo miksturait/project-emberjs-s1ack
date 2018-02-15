808c9f369185324d6a8398f7e5066bcd3709b1ad
BobrImperator <lordboberro@gmail.com>
2018-02-15 16:46:50 +0100

    add classNameBindings

--- app/components/single-message.js
+++ app/components/single-message.js
@@ -1,9 +1,12 @@
 import Component from '@ember/component';
 import { get } from '@ember/object';
+import { alias } from '@ember/object/computed';
 
 export default Component.extend({
   tagName: 'li',
   classNames: ['flex'],
+  classNameBindings: ['isPinned:pinned'],
+  isPinned: alias('msg.pinned'),
   didInsertElement() {
     this._super(...arguments);
     this.element.scrollIntoView();
