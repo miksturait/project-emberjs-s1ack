8779dbe1f9889f1777fa905c8e574c2132ba52e5
BobrImperator <lordboberro@gmail.com>
2018-02-15 16:22:46 +0100

    add pinned property to messages, channel filterBy pinned

--- app/models/channel.js
+++ app/models/channel.js
@@ -1,8 +1,10 @@
 import DS from 'ember-data';
+import { filterBy } from '@ember/object/computed';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   title: DS.attr('string'),
   messages: DS.hasMany('message'),
-  organization: DS.belongsTo('organization')
+  organization: DS.belongsTo('organization'),
+  pinnedMessages: filterBy('messages', 'pinned', true),
 });
--- app/models/message.js
+++ app/models/message.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   time: DS.attr('string'),
   content: DS.attr('string'),
+  pinned: DS.attr('boolean'),
   author: DS.belongsTo('user'),
   channel: DS.belongsTo('channel')
 });
